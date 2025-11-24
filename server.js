37348b6cde16793aad10c4b5c43eaae50a1858e0
ganesh-gandla <ganeshgandla19@gmail.com>
2025-11-24 20:47:09 +0530

    Integrated mongoose and Schema for product, user and cart item

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-
-const app = express();
-
-app.listen(5100,()=>{
-    console.log("The sever running on port 5100")
-})
-
---
+++ src/models/Product.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+}, { timestamps: true });
+
+export default mongoose.model("Product", productSchema);
